b8f07c50b7be8a64a0fc874d1a971b49f1ac9e0d
jesseTitus <jessejtitus@gmail.com>
2024-05-10 14:32:21 -0400

    updated menu, styling, about us

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -33,6 +33,7 @@ class Menu(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField(default=10)
     category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None, related_name="category_name")
+    description = models.CharField(max_length=200)
     def __str__(self):
         return f"{self.name} : {self.category_id}"
         
